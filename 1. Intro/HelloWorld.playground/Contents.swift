b6d6fb9abb4b761ae5281bf44ac6d945b5477bd9
carlkarama <carlkarama4@gmail.com>
2022-10-24 18:00:55 +1100

    feat: welcome to Swift
    The beginning of great things!

---
+++ 1. Intro/HelloWorld.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+/**
+ * var is a keyword to denote a value that needs to be changed
+ */
+var greeting = "Hello, world!"
+
+
+/**
+ * let is a keyowrd to denote a value that needs to be changed
+ */
+
+let dateOfBirth = "1/1/1900"
