2008a60ed2c9d66c7f225ba40071a9a0b2932a3c
carlkarama <carlkarama4@gmail.com>
2022-11-07 14:11:30 +1100

    ref: invert grading scheme
    Description: Update only results that leave a remainder of less than 3 when divided by 5
    Link: https://www.hackerrank.com/challenges/grading/problem

--- 3. DataStructures & Algorithms/DSA-2-GradingStudents.playground/Contents.swift
+++ 3. DataStructures & Algorithms/DSA-2-GradingStudents.playground/Contents.swift
@@ -8,20 +8,27 @@ import Foundation
 func gradingStudents(grades: [Int]) -> [Int] {
     
     // Write your code here
-    var tempArray = grades
+    var tempArray = [Int]()
     
     for grade in grades {
-        if grade % 5 < 3 {
-            tempArray.append(grade)
+        
+        if (grade % 5 > 3) {
+            let roundedGrade = grade % 5 + grade
+            tempArray.append(roundedGrade)
         }
         
         if grade < 38 {
-            print("Failed")
+            print("Failed", grade)
         }
         
-        if grade % 5 > 3 {
-            print("Cant round")
+        if grade % 5 < 3 {
+            print("Cant round", grade)
         }
     }
     return tempArray
 }
+
+
+var results:[Int] = [29, 44, 69, 91]
+
+print(gradingStudents(grades: results))
