cd624080486d5807d4a60df713e8c83b609d3c77
carlkarama <carlkarama4@gmail.com>
2022-11-09 19:06:47 +1100

    feat: inline closure implementation

---
+++ 1. Intro/7. Closures/ImpliedTypes.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+import Foundation
+
+
+// create search function that returns a boolean
+
+public func search(song track: String) -> Int {
+    
+    let songlist: [String] = ["Cannot wait", "Fullstop", "Ye"]
+    let tempTrack = track
+    
+    for searchingTrack in songlist {
+        if searchingTrack == tempTrack {
+            return 1
+        }
+    }
+    return 0
+}
+
+// create a variable that stores the letter we'll use to search through our song list
+
+var songStartsWith = "Y"
+
+// create a variable that can store the function
+
+
+// remove function name that you are passing in
